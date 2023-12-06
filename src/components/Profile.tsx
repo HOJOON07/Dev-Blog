715b1802e65717841bde45c404e9790810297c1d
hojoon <ghwns107@naver.com>
2023-12-07 03:51:40 +0900

    context api 로 auth 객체 만들기'

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,34 +1,34 @@
 import { log } from "console";
+import AuthContext from "context/AuthContext";
 import { getAuth, signOut } from "firebase/auth";
 import { app } from "firebaseApp";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
 
-export default function Profile() {
-  const auth = getAuth(app);
+const onSignOut = async () => {
+  try {
+    const auth = getAuth(app);
+    await signOut(auth);
+    toast.success("로그아웃 되었습니다.");
+  } catch (err: any) {
+    console.log(err);
+    toast.error(err?.code);
+  }
+};
 
-  const logout = async () => {
-    try {
-      const auth = getAuth(app);
-      await signOut(auth);
-      toast.success("로그아웃 되었습니다.");
-    } catch (err: any) {
-      console.log(err);
-      toast.error(err?.code);
-    }
-  };
+export default function Profile() {
+  const { user } = useContext(AuthContext);
   return (
     <div className="profile_box">
       <div className="flex_box-lg">
         <div className="profile_image"></div>
         <div>
-          <div className="profile_email">{auth?.currentUser?.email}</div>
-          <div className="profile_name">
-            {auth?.currentUser?.displayName || "사용자"}
-          </div>
+          <div className="profile_email">{user?.email}</div>
+          <div className="profile_name">{user?.displayName || "사용자"}</div>
         </div>
       </div>
-      <div role="presentation" className="profile_logout" onClick={logout}>
+      <div role="presentation" className="profile_logout" onClick={onSignOut}>
         로그아웃
       </div>
     </div>
