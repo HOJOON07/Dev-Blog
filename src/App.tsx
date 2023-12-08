a6e5da7274cfdcf98e8287afa8b293d4cde4c6dd
hojoon <ghwns107@naver.com>
2023-12-08 16:02:03 +0900

    context api 사용해서 다크모드 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import Router from "./components/Router";
 import { app, db } from "firebaseApp";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Loader from "components/Loader";
+import { ThemeContext } from "context/ThemeContext";
 
 function App() {
+  const context = useContext(ThemeContext);
+
   const auth = getAuth(app);
   // auth를 체크하기 전에 (initialize)전 에는 loader를 띄워주는 용도
   const [init, setInit] = useState<boolean>(false);
@@ -31,14 +34,14 @@ function App() {
   }, [auth]);
 
   return (
-    <>
+    <div className={context.theme === "light" ? "white" : "dark"}>
       <ToastContainer></ToastContainer>
       {init ? (
         <Router isAuthenticated={isAuthenticated}></Router>
       ) : (
         <Loader></Loader>
       )}
-    </>
+    </div>
   );
 }
 
